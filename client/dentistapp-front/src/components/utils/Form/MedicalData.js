b506e47190886a00356062a04fe19a9e660ce2be
LauraTDauden <lauratdauden@gmail.com>
2021-12-23 18:33:05 +0000

    created provisional subcomponents for form

---
+++ client/dentistapp-front/src/components/utils/Form/MedicalData.js
@@ -0,0 +1,111 @@
+/*
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+
+export const MedicalData = () => {
+
+    return (
+        <div>
+            <div>
+                <h3>
+                    <FormattedMessage
+                        id="form.pathology"
+                        defaultMessage=" ¿Ha tenido alguna patología oral previa o la presenta en la actualidad?"
+                    />
+                </h3>
+
+                <input
+                    type="radio"
+                    onChange={handleInputChange}
+                    value="true"
+                    name="pathology"
+                />
+                <label>
+                    <FormattedMessage
+                        id="form.yes"
+                        defaultMessage="Sí"
+                    />
+                </label>
+                <br />
+
+                <input type="radio"
+                    onChange={handleInputChange}
+                    value="false"
+                    name="pathology"
+                    defaultChecked />
+
+                <label>
+                    <FormattedMessage
+                        id="form.no"
+                        defaultMessage="No"
+                    />
+                </label>
+                <br />
+
+                <label>
+                    <FormattedMessage
+                        id="form.specify"
+                        defaultMessage="En caso afirmativo, especificar patologías:"
+                    />
+                </label>
+                <input type="text"
+                    onChange={handleInputChange}
+                    value={formData.descriptionPathology}
+                    name="descriptionPathology"
+                />
+                <br />
+            </div>
+            <hr />
+
+            <div>
+                <h3>
+                    <FormattedMessage
+                        id="form.allergy"
+                        defaultMessage="¿Es alérgico algún alimento/medicamento/producto?"
+                    />
+                </h3>
+
+                <input type="radio"
+                    onChange={handleInputChange}
+                    value="true"
+                    name="allergy"
+                />
+                <label >
+                    <FormattedMessage
+                        id="form.yes"
+                        defaultMessage="Sí"
+                    />
+                </label>
+                <br />
+
+                <input type="radio"
+                    onChange={handleInputChange}
+                    value="false"
+                    name="allergy"
+                    defaultChecked />
+                <label >
+                    <FormattedMessage
+                        id="form.no"
+                        defaultMessage="No"
+                    />
+                </label>
+                <br />
+
+                <label>
+                    <FormattedMessage
+                        id="form.specify"
+                        defaultMessage="En caso afirmativo, especificar alergias:"
+                    />
+                </label>
+                <input type="text"
+                    onChange={handleInputChange}
+                    value={formData.descriptionAllergy}
+                    name="descriptionAllergy"
+                />
+                <br />
+            </div>
+        </div>
+    )
+}
+*/
---
+++ client/dentistapp-front/src/components/utils/Form/PainLocation.js
@@ -0,0 +1,26 @@
+/*
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+
+export const PainLocation = () => {
+
+    return (
+        <div>
+            <h3>
+                <FormattedMessage
+                    id="form.location"
+                    defaultMessage="Localización del dolor:"
+                />
+            </h3>
+
+            <input type="text"
+                onChange={handleInputChange}
+                value={formData.pain_zone}
+                name="pain_zone"
+                placeholder="Aquí va una boca "
+            />
+        </div>
+    )
+}
+*/
