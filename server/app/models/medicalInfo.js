bdf59439fbe56568c953d7002b341410ce1ffa74
AdrielAO <adrielarochaoronoz@gmail.com>
2022-02-15 19:52:38 +0000

    adding realtion in models and method in controller

--- server/app/models/medicalInfo.js
+++ server/app/models/medicalInfo.js
@@ -19,6 +19,8 @@ module.exports = (sequelize, DataTypes) => {
       // medicalInfo.hasOne(models.Simptoms);
       medicalInfo.hasOne(models.simptoms);
       
+      medicalInfo.hasOne(models.Teeth)
+      
     }
   };
   medicalInfo.init({//todo add description to allergy and pathology
