4e78ccb3f0a9f5584cd06f5106dfd2093dbd14ba
LauraTDauden <lauratdauden@gmail.com>
2022-02-16 23:05:57 +0000

    arreglado estilo Not Found

--- client/dentistapp-front/src/components/NotFound.js
+++ client/dentistapp-front/src/components/NotFound.js
@@ -3,7 +3,7 @@ import '../assets/styles/notfound.css';
 
 export const NotFound = () => {
     return (
-        <div>
+        <div className = "notfound">
             <h1> 404 </h1>
             <h2> Page not found </h2>
         </div>
