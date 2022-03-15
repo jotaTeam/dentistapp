1c43b50e11c28aafdf6e314e4d99ee2f6521bd57
LauraTDauden <lauratdauden@gmail.com>
2022-03-15 17:18:40 +0000

    Removed unused reference

--- client/dentistapp-front/src/components/utils/Form/PainLocation.js
+++ client/dentistapp-front/src/components/utils/Form/PainLocation.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { FormattedMessage } from "react-intl";
 
 export const PainLocation = ({ setTeeth, active, setActive, prevState }) => {
