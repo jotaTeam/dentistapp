e5aa6a3485fbc4ca9b94a4784cec2e2eb118c997
wenesmek <wenesmek@gmail.com>
2022-03-16 03:49:23 +0000

    AdminHome - first list

--- client/dentistapp-front/src/components/admin/AdminHome.js
+++ client/dentistapp-front/src/components/admin/AdminHome.js
@@ -1,97 +1,170 @@
-import React, {useEffect } from 'react';
-import { apiUrl } from '../../datahelpers/apiURL';
-import { getDataList } from '../../crud/getDataList';
-import { getDataById } from '../../crud/getDataById';
-import { createData } from '../../crud/createData';
-import {useState} from 'react';
+import React, { useEffect } from "react";
+import { apiUrl } from "../../datahelpers/apiURL";
+import { getDataList } from "../../crud/getDataList";
+import { useState } from "react";
+import LanguageSelector from "../LanguageSelector";
 
 export const AdminHome = () => {
-
-   const [data1, setData1] = useState();
-   const [data2, setData2] = useState();
-   const [user, setUser] = useState();
-   const [user2, setUser2] = useState();
+  const [data1, setData1] = useState();
 
   useEffect(() => {
-     getDataList(apiUrl.patient)
-     .then(data => setData1(data.data));
-    getDataList(apiUrl.user)
-       .then(data=> setUser(data.data));
-    getDataById('a3', apiUrl.patient) 
-      .then(data => setData2(data.data));
-    getDataById('2', apiUrl.user) 
-      .then(data => setUser2(data.data));
+    getDataList(apiUrl.emergency).then((data) => setData1(data.data));
+  }, []);
 
-  },[]);
+  return (
+    <div style={{textAlign:"center"}}>
+      <LanguageSelector />
 
-
-  const pruebi = async() => {
-
-    const pruebi1 = await createData({name: 'testfinal', password: '1234', email: 'email@example.com', google_token: '12345678'}, apiUrl.user);
-
-    console.log(pruebi1)
-    
-     createData({name: 'otro pa', lastname: '1234', dni: 'a3'}, apiUrl.patient);
-   
-    const dataUrgencia = await createData({name: 'PruebaUno', surnames: 'Apellido', phone: 987653214, allergy: true, description_allergy: 'Prueba uno y tal', bleeding: true, pain: true, cavity: true, fracture: true, pieces: 'unos cuantos' }, apiUrl.emergency);
-
-       console.log(dataUrgencia);
-
-  }
-
-
-
-
-    return (
-        <div> 
-            <h1>Home del admin</h1>
-
-            <hr />
-
-            <h3>Listado pacientes:</h3>
-
-            {
-
-                data1 
-                    ? data1.map(patient => <p key={patient.dni}>{JSON.stringify(patient)}</p>)
-                    : <p>loading...</p>
-            }
-
-
-            <br />
-            <hr />
-            <h3>Paciente con el DNI nº3</h3>
-            {
-
-                data2
-                    ? <p >{JSON.stringify(data2)}</p>
-                    : <p>loading...</p>
-            }
-
-            <br />
-            <hr />
-            <h3>Listado de usuarios</h3>
-            {
-
-                user
-                    ? user.map(usertemp => <p key={usertemp.id}>{JSON.stringify(usertemp)}</p>)
-                    : <p>loading...</p>
-            }
-            <br />
-            <hr />
-            <h3>Usuario con el Id 2</h3>
-            {
-
-                user2
-                    ? <p >{JSON.stringify(user2)}</p>
-                    : <p>loading...</p>
-            }
-
-
-
-		      <button onClick={pruebi}>Pruebi</button>
-            
-
+	<section className="home-header" style={{marginBottom:"100px"}}>
+        <div className="home-title-cont">
+          <h1 className="home-title-left">Administración</h1>
+          <hr />
         </div>
-    );
+        <br />
+        <br />
+        <h2>Emergencias por tramitar:</h2>
+
+        <br />
+        <br />
+      </section>
+      {data1 && data1.ok && (
+        <table border="1" style={{ border: "none"}}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Teléfono</th>
+              <th>Alergias</th>
+              <th>Info Aler.</th>
+              <th>Patologías</th>
+              <th>Info Pat.</th>
+              <th>Síntomas</th>
+              <th>Causas</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data1.emergency.map((emer) => (
+              <tr key={emer.id}>
+                <td>{emer.id}</td>
+                {console.log(emer.medicalInfo.cause)}
+                <td>{emer.name}</td>
+                <td>{emer.surnames}</td>
+                <td>{emer.phone}</td>
+                <td>
+                  {emer.medicalInfo.allergy ? <span>Si</span> : <span>No</span>}
+                </td>
+                <td>{emer.medicalInfo.descriptionAllergy}</td>
+                <td>
+                  {emer.medicalInfo.pathology ? (
+                    <span>Si</span>
+                  ) : (
+                    <span>No</span>
+                  )}
+                </td>
+                <td>{emer.medicalInfo.descriptionPathology}</td>
+                <td>
+                  <table
+                    border="1"
+                    style={{ border: "none", borderCollapse: "collapse" }}
+                  >
+                    <thead>
+                      <tr>
+                        <th>Sangrado</th>
+                        <th>Dolor</th>
+                        <th>Movimiento dental</th>
+                        <th>Úlcera</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          {emer.medicalInfo.simptom.bleeding ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+                        <td>
+                          {emer.medicalInfo.simptom.pain ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+                        <td>
+                          {emer.medicalInfo.simptom.dental_movility ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+                        <td>
+                          {emer.medicalInfo.simptom.ulcer ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table
+                    border="1"
+                    style={{ border: "none", borderCollapse: "collapse" }}
+                  >
+                    <thead>
+                      <tr>
+                        <th>Caries</th>
+                        <th>Fractura</th>
+                        <th>Problemas de encías</th>
+                        <th>P. higiene oral</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td>
+                          {emer.medicalInfo.cause.cavity ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+
+                        <td>
+                          {emer.medicalInfo.cause.fracture ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+
+                        <td>
+                          {emer.medicalInfo.cause.gums_problems ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+                        <td>
+                          {emer.medicalInfo.cause.oral_hygiene ? (
+                            <span>Si</span>
+                          ) : (
+                            <span>No</span>
+                          )}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
